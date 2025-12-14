e7bc6986340a934f1b6ce4730cd22d85d15e6ef3
CHOW YI MIN <112329967+yimin04@users.noreply.github.com>
2025-12-14 23:07:27 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c5qyekcmk6ln.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chowyimin-employee"
+customregion = "us-east-1b"
+
